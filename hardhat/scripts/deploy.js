3876cd780ccd61f4131907a033aab635b9d2cc23
StealthElf98 <sergej.dragojlovic@gmail.com>
2024-10-02 20:48:07 +0200

    fix

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -11,6 +11,3 @@ main().catch((error) => {
     console.error(error)
     process.exitCode = 1
 })
-
-// Contract address: 0xA1057e079Db3e9afA5c1CDe2dFCa3e783627E3D7
-// https://amoy.polygonscan.com/address/0xA1057e079Db3e9afA5c1CDe2dFCa3e783627E3D7#code
